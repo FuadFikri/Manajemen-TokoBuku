458020468b0d3beaea0e7466b931558a50e44ebd
FuadFikri <fuadfikri01@gmail.com>
2018-11-09 22:14:51 +0700

    adminseeder-rev

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -14,12 +14,12 @@ class AdminSeeder extends Seeder
             $administrator = new \App\User;
             $administrator->username = "administrator";
             $administrator->name = "Site Administrator";
-            $administrator->email = "administrator@larashop.test";
+            $administrator->email = "admin@mail.com";
             $administrator->roles = json_encode(["ADMIN"]);
             $administrator->address = 'Benua Pogung';
             $administrator->phone = '089765678';
             $administrator->avatar = 'avatar aang';
-            $administrator->password = \Hash::make("larashop");
+            $administrator->password = \Hash::make("admin123");
             $administrator->save();
             $this->command->info("User Admin berhasil diinsert");
     }
